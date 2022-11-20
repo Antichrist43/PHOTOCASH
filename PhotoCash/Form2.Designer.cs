76bc61b374e44240a459cb86df5f01bdb25f3f7d
Genghis Khan <106480394+Antichrist43@users.noreply.github.com>
2022-11-20 08:49:44 -0600

    Add files via upload

---
+++ PhotoCash/Form2.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace PhotoCash
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fAQToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.networkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.photoCashChainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.photosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.myPhotosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sendCashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.receiveCashToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createWalletToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encryptWalletToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.networkToolStripMenuItem,
+            this.photosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.versionToolStripMenuItem,
+            this.fAQToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.versionToolStripMenuItem.Text = "Version";
+            // 
+            // fAQToolStripMenuItem
+            // 
+            this.fAQToolStripMenuItem.Name = "fAQToolStripMenuItem";
+            this.fAQToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fAQToolStripMenuItem.Text = "FAQ";
+            // 
+            // networkToolStripMenuItem
+            // 
+            this.networkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.photoCashChainToolStripMenuItem});
+            this.networkToolStripMenuItem.Name = "networkToolStripMenuItem";
+            this.networkToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.networkToolStripMenuItem.Text = "Network";
+            // 
+            // photoCashChainToolStripMenuItem
+            // 
+            this.photoCashChainToolStripMenuItem.Name = "photoCashChainToolStripMenuItem";
+            this.photoCashChainToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.photoCashChainToolStripMenuItem.Text = "PhotoCash Chain";
+            // 
+            // photosToolStripMenuItem
+            // 
+            this.photosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.myPhotosToolStripMenuItem});
+            this.photosToolStripMenuItem.Name = "photosToolStripMenuItem";
+            this.photosToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.photosToolStripMenuItem.Text = "Photos";
+            // 
+            // myPhotosToolStripMenuItem
+            // 
+            this.myPhotosToolStripMenuItem.Name = "myPhotosToolStripMenuItem";
+            this.myPhotosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.myPhotosToolStripMenuItem.Text = "My Photos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PhotoCash Earned:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 416);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Network: Online";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // sendCashToolStripMenuItem
+            // 
+            this.sendCashToolStripMenuItem.Name = "sendCashToolStripMenuItem";
+            this.sendCashToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sendCashToolStripMenuItem.Text = "Send Photocash";
+            // 
+            // receiveCashToolStripMenuItem
+            // 
+            this.receiveCashToolStripMenuItem.Name = "receiveCashToolStripMenuItem";
+            this.receiveCashToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.receiveCashToolStripMenuItem.Text = "Receive Photocash";
+            // 
+            // createWalletToolStripMenuItem
+            // 
+            this.createWalletToolStripMenuItem.Name = "createWalletToolStripMenuItem";
+            this.createWalletToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.createWalletToolStripMenuItem.Text = "Create Wallet";
+            // 
+            // encryptWalletToolStripMenuItem
+            // 
+            this.encryptWalletToolStripMenuItem.Name = "encryptWalletToolStripMenuItem";
+            this.encryptWalletToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.encryptWalletToolStripMenuItem.Text = "Encrypt Wallet";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.sendCashToolStripMenuItem,
+            this.receiveCashToolStripMenuItem,
+            this.createWalletToolStripMenuItem,
+            this.encryptWalletToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "PhotoCash Core";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem networkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fAQToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem photoCashChainToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem photosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem myPhotosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sendCashToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem receiveCashToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createWalletToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem encryptWalletToolStripMenuItem;
+    }
+}
